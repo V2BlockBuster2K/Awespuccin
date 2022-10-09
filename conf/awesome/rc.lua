448b9c8242628ea5fbcd68a255d67b05589c91ba
V2BlockBuster2K <84659429+V2BlockBuster2K@users.noreply.github.com>
2022-10-09 14:03:19 +0200

    Add files via upload

---
+++ conf/awesome/rc.lua
@@ -0,0 +1,23 @@
+pcall(require, "luarocks.loader")
+local gears = require("gears")
+local awful = require("awful")
+local beautiful = require("beautiful")
+require("awful.autofocus")
+-- Theme --
+beautiful.init(gears.filesystem.get_configuration_dir() .. "themes/catppuccin/theme.lua")
+
+-- Main/Configuration --
+require "main"
+
+-- Libraries/Modules --
+require "lib"
+
+-- Signals/Monitoring --
+require "signals"
+
+-- Ui/Panels --
+require "ui"
+
+-- Autostart --
+awful.spawn.with_shell("picom --experimental-backends")
+awful.spawn.with_shell("start-stop-daemon mpDris2 -q") -- OpenRC mpDris2 since no service file -- 
---
+++ conf/awesome/user_variables.lua
@@ -0,0 +1,29 @@
+return {
+	widget = {
+		weather = {
+			--- API Key
+			key = " ",
+			--- Coordinates
+			coordinates = {
+				" ", --- lat
+				" ", --- lon
+			},
+		},
+
+		--- Github activity
+		github = {
+			username = "V2BlockBuster2K",
+		},
+
+		--- Microphone 
+		mic = {
+			name = "alsa_input.pci-0000_00_1b.0.analog-stereo",
+		},
+
+		--- Disk for Top-panel
+		disk = {
+			name = "/dev/sda2",
+		},
+		
+	},
+}
